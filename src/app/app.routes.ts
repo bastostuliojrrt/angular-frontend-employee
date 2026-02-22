3fb85ed8005c27c0d8ffa8a7185fdb4ac7787c19
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-02-22 19:42:31 -0300

    feat: Configuração do roteamento do projeto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: 'login', pathMatch: 'full'}, // rota padrão que redireciona para a página de login
+    {path: 'login', component: LoginComponent}, // rota para a página de login
+    {path: 'employees', component: EmployeeListComponent}
+
+];
+
