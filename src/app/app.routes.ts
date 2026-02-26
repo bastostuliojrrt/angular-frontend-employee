87a5d4f3ebdede7f804128c7236c6d2998c443e4
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-02-26 10:34:22 -0300

    Feat: ajuste no path employees para que aponte para o componente EmployeesComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
 
 export const routes: Routes = [
 
     {path: '', redirectTo: 'login', pathMatch: 'full'}, // rota padrão que redireciona para a página de login
     {path: 'login', component: LoginComponent}, // rota para a página de login
-    {path: 'employees', component: EmployeeListComponent}
+    {path: 'employees', component: EmployeesComponent}
 
 ];
 
