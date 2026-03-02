5c1e2f24e4f39ec964c1936e63e8ecf50eeb4ab2
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-03-02 00:56:54 -0300

    feat: criação do formulário reativo para cadastro de employees e ajuste no component primary-input para que ele funcione dentro do formulário reativo.

--- src/app/components/create-employee/create-employee.component.ts
+++ src/app/components/create-employee/create-employee.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { Employee } from '../../employee';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from "../primary-input/primary-input.component";
 
 @Component({
   selector: 'app-create-employee',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+],
   templateUrl: './create-employee.component.html',
   styleUrl: './create-employee.component.css'
 })
 export class CreateEmployeeComponent {
+
+  employee: Employee = new Employee();
+
+  createEmployeeForm!: FormGroup;
+
+  constructor() { 
+    this.createEmployeeForm = new FormGroup({
+      emailId: new FormControl('', [Validators.required, Validators.email]),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      departamentId: new FormControl('', Validators.required)
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.createEmployeeForm.value);
+  }
+
 
 }
---
+++ src/app/components/primary-input/primary-input.component.ts
@@ -0,0 +1,52 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { Employee } from '../../employee';
+
+type InputTypes = 'text' | 'email' | 'password' | 'number' | 'date' | 'tel' | 'url';
+
+@Component({
+  selector: 'app-primary-input',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PrimaryInputComponent),
+      multi: true
+    }
+  ],
+  templateUrl: './primary-input.component.html',
+  styleUrl: './primary-input.component.css'
+})
+export class PrimaryInputComponent implements ControlValueAccessor{
+
+  employee: Employee = new Employee();
+
+  @Input() type: InputTypes = 'text';
+
+  value: string ='';
+  onChange: any = () => {};
+  onTouched: any = () => {};
+  
+  onInput(event: Event){
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value)
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    
+  }
+
+}
