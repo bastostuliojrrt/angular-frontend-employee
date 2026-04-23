026a7c54d70a280623ebca59ea0b9698af70d99d
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-04-23 03:11:55 -0300

    feat: implementação da rota de update do employee

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { EmployeesComponent } from './pages/employees/employees.component';
 import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
 
 export const routes: Routes = [
 
     {path: '', redirectTo: 'login', pathMatch: 'full'}, // rota padrão que redireciona para a página de login
     {path: 'login', component: LoginComponent}, // rota para a página de login
     {path: 'employees', component: EmployeesComponent},
-    {path: 'create-employee', component: CreateEmployeeComponent}
+    {path: 'create-employee', component: CreateEmployeeComponent},
+    {path: 'update-employee/:id', component: UpdateEmployeeComponent}
 
 ];
 
