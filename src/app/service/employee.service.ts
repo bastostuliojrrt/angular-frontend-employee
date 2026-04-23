0dff0d0c6ef8fcec27c092dc911f40fec2f30634
bastostuliojrrt <bastostuliojrrt@gmail.com>
2026-04-23 04:21:07 -0300

    feat: implementação do método de update "updateEmployee".

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -26,4 +26,8 @@ export class EmployeeService {
     return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
   }
 
+  updateEmployee(id: number, employee: Employee): Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${id}`, employee);
+  }
+
 }
